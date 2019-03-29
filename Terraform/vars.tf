af336bcd9375033a11320958de7537fec2ebfe9a
Mayur Barge <bargemayur05@gmail.com>
2019-03-29 10:12:13 -0700

    Terraform - Adding missing components like NAT instance and modifications to route table

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,44 +1,44 @@
 variable "aws_region" {
   description = "Region for the VPC"
-  default = "us-west-2"
+  default = "us-east-2"
 }
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
-  default = "10.0.0.0/16"
+  default = "10.1.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
-  default = "10.0.0.0/24"
+  default = "10.1.0.0/24"
 }
 
 variable "public_subnet_az" {
   description = "AZ for the public subnet"
-  default = "us-west-2a"
+  default = "us-east-2a"
 }
 
 variable "private_subnet_1_cidr" {
   description = "CIDR for the private subnet"
-  default = "10.0.1.0/24"
+  default = "10.1.1.0/24"
 }
 
 variable "private_subnet_1_az" {
   description = "AZ for the private subnet 1"
-  default = "us-west-2b"
+  default = "us-east-2b"
 }
 
 variable "private_subnet_2_cidr" {
   description = "CIDR for the private subnet 2"
-  default = "10.0.2.0/24"
+  default = "10.1.2.0/24"
 }
 
 variable "private_subnet_2_az" {
   description = "AZ for the private subnet 2"
-  default = "us-west-2c"
+  default = "us-east-2c"
 }
 
-variable "key_path" {
-  description = "SSH Public Key path"
-  default = "/home/core/.ssh/id_rsa.pub"
+variable "aws_key_name" {
+    description = "Key name for NAT instance"
+    default = "mayur"
 }
